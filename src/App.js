e998385d8813c13d0af59e64476919d2e3b2f861
mr siddiqui <shahrukhsiddiqui9819@gmail.com>
2024-05-02 01:33:13 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import  Navbar from "./components/navbar";
+import B from "./components/body";
+import Bar from "./components/bar";
+import Nepale from "./components/slider";
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <B />
+      <Bar />
+      <Nepale />
+   </>
+  );
+}
